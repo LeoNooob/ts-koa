7563088e9096819d9bfd7046eaa7a0aa2fbcec33
XULONG LIU <xaskleowhyl@163.com>
2021-10-22 08:55:06 +0800

    feat: 加入数据库配置

--- app/config/index.ts
+++ app/config/index.ts
@@ -3,7 +3,10 @@ const config = {
         port: process.env.SERVER_PORT
     },
     db: {
-
+        db_host: process.env.DB_HOST,
+        db_name: process.env.DB_NAME,
+        db_user: process.env.DB_USER,
+        db_port: process.env.DB_PORT
     },
     log: {
         appenders: { 
